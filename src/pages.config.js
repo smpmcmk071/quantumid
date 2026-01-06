981938108c3d3647aada3c32a8ebda61031e47a1
Stephen Maher <smaher500@gmail.com>
2026-01-06 14:37:14 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,32 +1,6 @@
 import AboutNumerology from './pages/AboutNumerology';
-import AddFamilyMember from './pages/AddFamilyMember';
-import AdminNumerology from './pages/AdminNumerology';
-import AmyLegacy from './pages/AmyLegacy';
-import BrothersLegacy from './pages/BrothersLegacy';
-import CalendarEvents from './pages/CalendarEvents';
 import Candidates from './pages/Candidates';
-import ChristianLegacy from './pages/ChristianLegacy';
-import Community from './pages/Community';
 import Dashboard from './pages/Dashboard';
-import FamilyLegacy from './pages/FamilyLegacy';
-import FamilyTree from './pages/FamilyTree';
-import Games from './pages/Games';
-import GenealogyResearch from './pages/GenealogyResearch';
-import GrandparentsLegacy from './pages/GrandparentsLegacy';
-import Home from './pages/Home';
-import JoinFamily from './pages/JoinFamily';
-import KyleLegacy from './pages/KyleLegacy';
-import Landing from './pages/Landing';
-import MaherHistory from './pages/MaherHistory';
-import MaherLegacy from './pages/MaherLegacy';
-import MarketingPage from './pages/MarketingPage';
-import MelanieLegacy from './pages/MelanieLegacy';
-import NumerologyBattle from './pages/NumerologyBattle';
-import NumerologyBlackjack from './pages/NumerologyBlackjack';
-import NumerologyWar from './pages/NumerologyWar';
-import ParentsLegacy from './pages/ParentsLegacy';
-import PersonalDashboard from './pages/PersonalDashboard';
-import StephenLegacy from './pages/StephenLegacy';
 import Teams from './pages/Teams';
 import Analyzer from './pages/Analyzer';
 import JobPostings from './pages/JobPostings';
@@ -37,34 +11,8 @@ import __Layout from './Layout.jsx';
 
 export const PAGES = {
     "AboutNumerology": AboutNumerology,
-    "AddFamilyMember": AddFamilyMember,
-    "AdminNumerology": AdminNumerology,
-    "AmyLegacy": AmyLegacy,
-    "BrothersLegacy": BrothersLegacy,
-    "CalendarEvents": CalendarEvents,
     "Candidates": Candidates,
-    "ChristianLegacy": ChristianLegacy,
-    "Community": Community,
     "Dashboard": Dashboard,
-    "FamilyLegacy": FamilyLegacy,
-    "FamilyTree": FamilyTree,
-    "Games": Games,
-    "GenealogyResearch": GenealogyResearch,
-    "GrandparentsLegacy": GrandparentsLegacy,
-    "Home": Home,
-    "JoinFamily": JoinFamily,
-    "KyleLegacy": KyleLegacy,
-    "Landing": Landing,
-    "MaherHistory": MaherHistory,
-    "MaherLegacy": MaherLegacy,
-    "MarketingPage": MarketingPage,
-    "MelanieLegacy": MelanieLegacy,
-    "NumerologyBattle": NumerologyBattle,
-    "NumerologyBlackjack": NumerologyBlackjack,
-    "NumerologyWar": NumerologyWar,
-    "ParentsLegacy": ParentsLegacy,
-    "PersonalDashboard": PersonalDashboard,
-    "StephenLegacy": StephenLegacy,
     "Teams": Teams,
     "Analyzer": Analyzer,
     "JobPostings": JobPostings,
